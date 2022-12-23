79114497d3a608121f2e23e8fa7e8d75313ac7c5
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-12-23 20:41:45 +0100

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,81 @@
 import "./App.css";
-import cytoscape from "cytoscape";
+import cytoscape, { CytoscapeOptions } from "cytoscape";
 import { useEffect } from "react";
 
+const nrOfNodes = 10;
+
+const generateNodes = () => {
+  return new Array(nrOfNodes).fill(0).map((v, i) => ({
+    data: { id: `a${i}` },
+  }));
+};
+
+// a -> b -> c
+const generateLineairEdges = (): any => {
+  const f = new Array(nrOfNodes - 1).fill(0).map((v, i) => ({
+    data: { id: `ab${i}`, source: `a${i}`, target: `a${i + 1}` },
+  }));
+  return f;
+};
+
+// c <- a -> b
+const generateHierachicalEdges = (): any => {
+  const firstHalf = nrOfNodes / 2;
+
+  const f = new Array(firstHalf - 1).fill(0).map((v, i) => ({
+    data: { id: `ab${i}`, source: `a${i}`, target: `a${i + 1}` },
+  }));
+
+  const f2 = new Array(firstHalf).fill(0).map((v, i) => ({
+    data: { id: `ba${i}`, source: `a0`, target: `a${i + firstHalf}` },
+  }));
+
+  const f3 = f.concat(f2);
+  console.log(f3);
+  return f3;
+};
+
+const gridLayout: CytoscapeOptions["layout"] = {
+  name: "grid",
+  rows: 2,
+};
+
+const randomLayout: CytoscapeOptions["layout"] = {
+  name: "random",
+  fit: true, // whether to fit to viewport
+  padding: 30, // fit padding
+  boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  animate: false, // whether to transition the node positions
+  animationDuration: 500, // duration of animation in ms if enabled
+  animationEasing: undefined, // easing of animation if enabled
+  animateFilter: function (node, i) {
+    return true;
+  }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+  ready: undefined, // callback on layoutready
+  stop: undefined, // callback on layoutstop
+  transform: function (node, position) {
+    return position;
+  }, // transform a given node position. Useful for changing flow direction in discrete layouts
+};
+
+const nullLayout: CytoscapeOptions["layout"] = {
+  name: "null",
+  ready: function () {}, // on layoutready
+  stop: function () {}, // on layoutstop
+};
+
 function App() {
+  const elements = [
+    ...generateNodes(),
+    // ...generateLineairEdges(),
+    ...generateHierachicalEdges(),
+  ];
+
   useEffect(() => {
     // const cy =
     cytoscape({
       container: document.getElementById("cy"), // container to render in
-      elements: [
-        // list of graph elements to start with
-        {
-          // node a
-          data: { id: "a" },
-        },
-        {
-          // node b
-          data: { id: "b" },
-        },
-        {
-          // edge ab
-          data: { id: "ab", source: "a", target: "b" },
-        },
-      ],
-
+      elements,
       style: [
         // the stylesheet for the graph
         {
@@ -46,10 +99,7 @@ function App() {
         },
       ],
 
-      layout: {
-        name: "grid",
-        rows: 1,
-      },
+      layout: gridLayout,
     });
   }, []);
 
