677b3f736b33a0ec23e8dc58069aff20a15f60fc
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-12-23 16:37:13 +0100

    add basic graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import cytoscape from "cytoscape";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // const cy =
+    cytoscape({
+      container: document.getElementById("cy"), // container to render in
+      elements: [
+        // list of graph elements to start with
+        {
+          // node a
+          data: { id: "a" },
+        },
+        {
+          // node b
+          data: { id: "b" },
+        },
+        {
+          // edge ab
+          data: { id: "ab", source: "a", target: "b" },
+        },
+      ],
+
+      style: [
+        // the stylesheet for the graph
+        {
+          selector: "node",
+          style: {
+            color: "white",
+            "background-color": "#4e4e4e",
+            label: "data(id)",
+          },
+        },
+
+        {
+          selector: "edge",
+          style: {
+            width: 3,
+            "line-color": "#ccc",
+            "target-arrow-color": "#ccc",
+            "target-arrow-shape": "triangle",
+            "curve-style": "bezier",
+          },
+        },
+      ],
+
+      layout: {
+        name: "grid",
+        rows: 1,
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="cy"></div>
     </div>
   );
 }
